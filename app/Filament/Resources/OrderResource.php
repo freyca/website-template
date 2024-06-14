1282219ae8ab09c322eee806b6fc120e998840d2
Fran Rey <fran.rc@raiolanetworks.es>
2024-06-14 14:50:03 +0000

    add: create orders

--- app/Filament/Resources/OrderResource.php
+++ app/Filament/Resources/OrderResource.php
@@ -8,7 +8,12 @@ use App\Enums\OrderStatus;
 use App\Enums\PaymentMethods;
 use App\Filament\Resources\OrderResource\Pages;
 use App\Models\Order;
+use App\Models\Product;
+use App\Models\ProductComplement;
+use App\Models\ProductSparePart;
 use Filament\Forms;
+use Filament\Forms\Components\Actions\Action;
+use Filament\Forms\Components\Repeater;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
@@ -24,22 +29,36 @@ class OrderResource extends Resource
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('purchase_cost')
-                    ->required()
-                    ->numeric(),
-                Forms\Components\Select::make('payment_method')
-                    ->required()
-                    ->options(PaymentMethods::class),
-                Forms\Components\Select::make('status')
-                    ->required()
-                    ->options(OrderStatus::class),
-                Forms\Components\Select::make('user_id')
-                    ->required()
-                    ->relationship('user', 'name'),
-                Forms\Components\Select::make('products')
-                    ->required()
-                    ->multiple()
-                    ->relationship('products', 'name'),
+                Forms\Components\Section::make([
+                    Forms\Components\Select::make('user_id')
+                        ->required()
+                        ->relationship('user', 'name')
+                        ->label('Customer'),
+                    Forms\Components\Select::make('payment_method')
+                        ->required()
+                        ->options(PaymentMethods::class),
+                    Forms\Components\TextInput::make('purchase_cost')
+                        ->label('Price')
+                        ->required()
+                        ->numeric(),
+                    Forms\Components\ToggleButtons::make('status')
+                        ->inline()
+                        ->options(OrderStatus::class)
+                        ->required()
+                        ->columnSpan('full'),
+                ])->columns(2),
+
+                Forms\Components\Section::make([
+                    static::getProductsRepeater(),
+                ]),
+
+                Forms\Components\Section::make([
+                    static::getProductComplementsRepeater(),
+                ]),
+
+                Forms\Components\Section::make([
+                    static::getProductSparePartsRepeater(),
+                ]),
             ]);
     }
 
@@ -80,5 +99,188 @@ class OrderResource extends Resource
             'create' => Pages\CreateOrder::route('/create'),
             'edit' => Pages\EditOrder::route('/{record}/edit'),
         ];
+    }
+
+    public static function getProductsRepeater(): Repeater
+    {
+        return Repeater::make('products')
+            ->relationship()
+            ->schema([
+                Forms\Components\Select::make('shop_product_id')
+                    ->label('Products')
+                    ->options(Product::query()->pluck('name', 'id'))
+                    ->required()
+                    ->reactive()
+                    ->afterStateUpdated(fn ($state, Forms\Set $set) => $set('unit_price', Product::find($state)?->price ?? 0))
+                    ->distinct()
+                    ->disableOptionsWhenSelectedInSiblingRepeaterItems()
+                    ->columnSpan([
+                        'md' => 5,
+                    ])
+                    ->searchable(),
+
+                Forms\Components\TextInput::make('qty')
+                    ->label('Quantity')
+                    ->numeric()
+                    ->default(1)
+                    ->columnSpan([
+                        'md' => 2,
+                    ])
+                    ->required(),
+
+                Forms\Components\TextInput::make('unit_price')
+                    ->label('Unit Price')
+                    ->disabled()
+                    ->dehydrated()
+                    ->numeric()
+                    ->required()
+                    ->columnSpan([
+                        'md' => 3,
+                    ]),
+            ])
+            ->extraItemActions([
+                Action::make('openProduct')
+                    ->tooltip('Open product')
+                    ->icon('heroicon-m-arrow-top-right-on-square')
+                    ->url(function (array $arguments, Repeater $component): ?string {
+                        $itemData = $component->getRawItemState($arguments['item']);
+
+                        $product = Product::find($itemData['shop_product_id']);
+
+                        if (! $product) {
+                            return null;
+                        }
+
+                        return ProductResource::getUrl('edit', ['record' => $product]);
+                    }, shouldOpenInNewTab: true)
+                    ->hidden(fn (array $arguments, Repeater $component): bool => blank($component->getRawItemState($arguments['item'])['shop_product_id'])),
+            ])
+            ->defaultItems(1)
+            ->columns([
+                'md' => 10,
+            ])
+            ->required();
+    }
+
+    public static function getProductComplementsRepeater(): Repeater
+    {
+        return Repeater::make('productComplements')
+            ->relationship()
+            ->schema([
+                Forms\Components\Select::make('shop_product_id')
+                    ->label('Product Complements')
+                    ->options(ProductComplement::query()->pluck('name', 'id'))
+                    ->required()
+                    ->reactive()
+                    ->afterStateUpdated(fn ($state, Forms\Set $set) => $set('unit_price', ProductComplement::find($state)?->price ?? 0))
+                    //->distinct()
+                    //->disableOptionsWhenSelectedInSiblingRepeaterItems()
+                    ->columnSpan([
+                        'md' => 5,
+                    ])
+                    ->searchable(),
+
+                Forms\Components\TextInput::make('qty')
+                    ->label('Quantity')
+                    ->numeric()
+                    ->default(1)
+                    ->columnSpan([
+                        'md' => 2,
+                    ])
+                    ->required(),
+
+                Forms\Components\TextInput::make('unit_price')
+                    ->label('Unit Price')
+                    ->disabled()
+                    ->dehydrated()
+                    ->numeric()
+                    ->required()
+                    ->columnSpan([
+                        'md' => 3,
+                    ]),
+            ])
+            ->extraItemActions([
+                Action::make('openProduct')
+                    ->tooltip('Open product')
+                    ->icon('heroicon-m-arrow-top-right-on-square')
+                    ->url(function (array $arguments, Repeater $component): ?string {
+                        $itemData = $component->getRawItemState($arguments['item']);
+
+                        $product = Product::find($itemData['shop_product_id']);
+
+                        if (! $product) {
+                            return null;
+                        }
+
+                        return ProductResource::getUrl('edit', ['record' => $product]);
+                    }, shouldOpenInNewTab: true)
+                    ->hidden(fn (array $arguments, Repeater $component): bool => blank($component->getRawItemState($arguments['item'])['shop_product_id'])),
+            ])
+            ->defaultItems(1)
+            ->columns([
+                'md' => 10,
+            ])
+            ->required();
+    }
+
+    public static function getProductSparePartsRepeater(): Repeater
+    {
+        return Repeater::make('productSpareParts')
+            ->relationship()
+            ->schema([
+                Forms\Components\Select::make('shop_product_id')
+                    ->label('Product Spare Parts')
+                    ->options(ProductSparePart::query()->pluck('name', 'id'))
+                    ->required()
+                    ->reactive()
+                    ->afterStateUpdated(fn ($state, Forms\Set $set) => $set('unit_price', ProductSparePart::find($state)?->price ?? 0))
+                    //->distinct()
+                    //->disableOptionsWhenSelectedInSiblingRepeaterItems()
+                    ->columnSpan([
+                        'md' => 5,
+                    ])
+                    ->searchable(),
+
+                Forms\Components\TextInput::make('qty')
+                    ->label('Quantity')
+                    ->numeric()
+                    ->default(1)
+                    ->columnSpan([
+                        'md' => 2,
+                    ])
+                    ->required(),
+
+                Forms\Components\TextInput::make('unit_price')
+                    ->label('Unit Price')
+                    ->disabled()
+                    ->dehydrated()
+                    ->numeric()
+                    ->required()
+                    ->columnSpan([
+                        'md' => 3,
+                    ]),
+            ])
+            ->extraItemActions([
+                Action::make('openProduct')
+                    ->tooltip('Open product')
+                    ->icon('heroicon-m-arrow-top-right-on-square')
+                    ->url(function (array $arguments, Repeater $component): ?string {
+                        $itemData = $component->getRawItemState($arguments['item']);
+
+                        $product = Product::find($itemData['shop_product_id']);
+
+                        if (! $product) {
+                            return null;
+                        }
+
+                        return ProductResource::getUrl('edit', ['record' => $product]);
+                    }, shouldOpenInNewTab: true)
+                    ->hidden(fn (array $arguments, Repeater $component): bool => blank($component->getRawItemState($arguments['item'])['shop_product_id'])),
+            ])
+            ->defaultItems(1)
+            ->columns([
+                'md' => 10,
+            ])
+            ->required();
     }
 }
