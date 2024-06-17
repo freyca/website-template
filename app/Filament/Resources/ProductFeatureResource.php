f40884a1fc0038a757008291ec44ba2bb52c8a24
Fran Rey <fran.rc@raiolanetworks.es>
2024-06-17 15:14:27 +0000

    add: resource for features

---
+++ app/Filament/Resources/ProductFeatureResource.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ProductFeatureResource\Pages;
+use App\Models\ProductFeature;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class ProductFeatureResource extends Resource
+{
+    protected static ?string $model = ProductFeature::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Section::make()->schema([
+                    Forms\Components\TextInput::make('name')
+                        ->required()
+                        ->maxLength(255),
+                    Forms\Components\MarkdownEditor::make('description')
+                        ->required()
+                        ->columnSpan('full')
+                        ->disableToolbarButtons([
+                            'attachFiles',
+                            'table',
+                        ]),
+                ]),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProductFeatures::route('/'),
+            'create' => Pages\CreateProductFeature::route('/create'),
+            'edit' => Pages\EditProductFeature::route('/{record}/edit'),
+        ];
+    }
+}
