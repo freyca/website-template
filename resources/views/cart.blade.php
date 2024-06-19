5755481d3c517486f445f99435f681a1550962bb
Fran Rey <fran.rc@raiolanetworks.es>
2024-06-19 16:35:28 +0000

    fix: correct component

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -5,9 +5,5 @@
 @section('main-content')
     @inject('cart', 'App\Services\Cart')
 
-    @if($cart->isEmpty())
-        <p class="container mx-auto">No products in cart</p>
-    @else
-        @livewire('cart-items', ['cartItems' => $cart->getCart()])
-    @endif
+    @livewire('cart-items', ['cartItems' => $cart->getCart()])
 @endsection
--- resources/views/livewire/cart-items.blade.php
+++ resources/views/livewire/cart-items.blade.php
@@ -1,14 +1,20 @@
 <div>
-    <div class="container mx-auto my-auto columns-3">
-        <span>{{ __('Product name') }}</span>
-        <span>{{ __('Quantity') }}</span>
-        <span>{{ __('Price') }}</span>
-    </div>
+    @inject('cart', 'App\Services\Cart');
 
-    @foreach ( $cartItems as $cartItem )
-        <x-show-product-in-cart
-            :product="data_get($cartItem, 'product')"
-            :quantity="data_get($cartItem, 'quantity')"
-        />
-    @endforeach
+    @if($cart->isEmpty())
+        <p class="container mx-auto">No products in cart</p>
+    @else
+        <div class="container mx-auto my-auto columns-3">
+            <span>{{ __('Product name') }}</span>
+            <span>{{ __('Quantity') }}</span>
+            <span>{{ __('Price') }}</span>
+        </div>
+
+        @foreach ( $cartItems as $cartItem )
+            <x-show-product-in-cart
+                :product="data_get($cartItem, 'product')"
+                :quantity="data_get($cartItem, 'quantity')"
+            />
+        @endforeach
+    @endif
 </div>
