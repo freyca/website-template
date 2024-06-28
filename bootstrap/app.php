9e542ef12f331d07770d47c135b9612bf5ec3d6b
Fran Rey <fran.rc@raiolanetworks.es>
2024-06-28 20:14:01 +0000

    fix: better middleware management

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,21 +1,13 @@
 <?php
 
-use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
-use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull;
-use Illuminate\Foundation\Http\Middleware\PreventRequestsDuringMaintenance;
 use Illuminate\Foundation\Http\Middleware\TrimStrings;
 use Illuminate\Foundation\Http\Middleware\ValidatePostSize;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Http\Middleware\HandleCors;
-use Illuminate\Http\Middleware\TrustProxies;
-use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\AuthenticateSession;
-use Illuminate\Session\Middleware\StartSession;
-use Illuminate\View\Middleware\ShareErrorsFromSession;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -24,20 +16,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->appendToGroup('frontend', [
-            TrustProxies::class,
+        $middleware->append([
             HandleCors::class,
-            PreventRequestsDuringMaintenance::class,
             ValidatePostSize::class,
             TrimStrings::class,
-            ConvertEmptyStringsToNull::class,
-            EncryptCookies::class,
-            AddQueuedCookiesToResponse::class,
-            StartSession::class,
             AuthenticateSession::class,
-            ShareErrorsFromSession::class,
-            VerifyCsrfToken::class,
-            SubstituteBindings::class,
+            //VerifyCsrfToken::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
--- routes/web.php
+++ routes/web.php
@@ -7,54 +7,52 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\SeoController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('frontend')->group(function () {
-    Route::get(
-        '/',
-        [HomeController::class, 'index']
-    )->name('home');
+Route::get(
+    '/',
+    [HomeController::class, 'index']
+)->name('home');
 
-    Route::get('quienes-somos', function () {
-        return view('who-we-are');
-    })->name('who-we-are');
+Route::get('quienes-somos', function () {
+    return view('who-we-are');
+})->name('who-we-are');
 
-    Route::get('como-comprar', function () {
-        return view('how-to-buy');
-    })->name('how-to-buy');
+Route::get('como-comprar', function () {
+    return view('how-to-buy');
+})->name('how-to-buy');
 
-    Route::get('sobre-nosotros', function () {
-        return view('about-us');
-    })->name('about-us');
+Route::get('sobre-nosotros', function () {
+    return view('about-us');
+})->name('about-us');
 
-    Route::get('contacto', function () {
-        return view('contact');
-    })->name('contact');
+Route::get('contacto', function () {
+    return view('contact');
+})->name('contact');
 
-    /** Cart */
-    Route::prefix('carrito')->name('cart.')->group(function () {
-        Route::get('/', function () {
-            return view('cart');
-        });
-
-        Route::get('products', [CartController::class, 'index'])->name('index');
-        Route::put('{product}', [CartController::class, 'store'])->name('add');
-        Route::post('clear', [CartController::class, 'clear'])->name('clear');
-        Route::delete('{product}', [CartController::class, 'delete'])->name('delete-product');
+/** Cart */
+Route::prefix('carrito')->name('cart.')->group(function () {
+    Route::get('/', function () {
+        return view('cart');
     });
 
-    /** Products */
-    Route::get('/productos', [ProductController::class, 'all'])->name('product-list');
-    Route::get('producto/{product}', [ProductController::class, 'product'])->name('product');
-    Route::get('/complementos-producto', [ProductController::class, 'complements'])->name('complement-list');
-    Route::get('complemento/{productComplement}', [ProductController::class, 'productComplement'])->name('complement');
-    Route::get('/piezas-de-repuesto', [ProductController::class, 'spareParts'])->name('spare-part-list');
-    Route::get('pieza-de-repuesto/{productSparePart}', [ProductController::class, 'productSparePart'])->name('spare-part');
-
-    /** Seo URL's */
-    Route::name('seo.')->group(function () {
-        Route::get('/desbrozadoras-por-menos-de-1000-euros', [SeoController::class, 'desbrozadorasBaratas'])->name('desbrozadoras-baratas');
-    });
-
-    /** Categories */
-    Route::get('categorias', [CategoryController::class, 'index'])->name('category-list');
-    Route::get('{category}', [CategoryController::class, 'category'])->name('category');
+    Route::get('products', [CartController::class, 'index'])->name('index');
+    Route::put('{product}', [CartController::class, 'store'])->name('add');
+    Route::post('clear', [CartController::class, 'clear'])->name('clear');
+    Route::delete('{product}', [CartController::class, 'delete'])->name('delete-product');
 });
+
+/** Products */
+Route::get('/productos', [ProductController::class, 'all'])->name('product-list');
+Route::get('producto/{product}', [ProductController::class, 'product'])->name('product');
+Route::get('/complementos-producto', [ProductController::class, 'complements'])->name('complement-list');
+Route::get('complemento/{productComplement}', [ProductController::class, 'productComplement'])->name('complement');
+Route::get('/piezas-de-repuesto', [ProductController::class, 'spareParts'])->name('spare-part-list');
+Route::get('pieza-de-repuesto/{productSparePart}', [ProductController::class, 'productSparePart'])->name('spare-part');
+
+/** Seo URL's */
+Route::name('seo.')->group(function () {
+    Route::get('/desbrozadoras-por-menos-de-1000-euros', [SeoController::class, 'desbrozadorasBaratas'])->name('desbrozadoras-baratas');
+});
+
+/** Categories */
+Route::get('categorias', [CategoryController::class, 'index'])->name('category-list');
+Route::get('{category}', [CategoryController::class, 'category'])->name('category');
