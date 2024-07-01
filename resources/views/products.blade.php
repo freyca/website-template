e999b372cd0624a7334214c8412bb0799cae4e4c
Fran Rey <fran.rc@raiolanetworks.es>
2024-07-01 15:24:25 +0000

    add: components ftw

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -5,14 +5,7 @@
     <h1 class="text-3xl font-semibold text-gray-800 mb-4">Productos</h1>
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         @foreach ($products as $product)
-            <div class="producto bg-white shadow-md rounded-lg overflow-hidden hover:shadow-lg transition-shadow duration-300">
-                <img src="{{ asset('public/images/' . $product->imagen) }}" alt="{{ $product->nombre }}" class="w-full h-48 object-cover">
-                <div class="p-4">
-                    <h2 class="text-xl font-semibold text-gray-800">{{ $product->nombre }}</h2>
-                    <p class="text-gray-600 mt-2">{{ $product->descripcion }}</p>
-                    <p class="text-gray-800 font-bold mt-2">{{ $product->precio }} &euro;</p>
-                </div>
-            </div>
+            <x-product.product-card :product="$product" />
         @endforeach
     </div>
 </div>
