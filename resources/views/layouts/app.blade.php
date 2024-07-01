6a1eac0daed860fba0d991d50dc63c33b403032f
Fran Rey <fran.rc@raiolanetworks.es>
2024-07-01 15:53:20 +0000

    fix: do not open body twice

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,20 +5,17 @@
     @include('partials.head')
 </head>
 
-<body class="bg-gray-100">
+<body class="bg-gray-100 text-gray-900">
 
-    <body class="bg-gray-100 text-gray-900">
     <x-navbar.navbar />
 
     <div class="container mx-auto mt-8">
-    <x-main-content-area />
-
+        <x-main-content-area />
     </div>
+
     <x-footer.footer />
 
     @vite('resources/js/app.js')
 </body>
+
 </html>
-
-
-
