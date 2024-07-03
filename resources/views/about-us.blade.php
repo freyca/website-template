f1d8322ae49944e647dd00b863ad7fddf252bafa
Fran Rey <fran.rc@raiolanetworks.es>
2024-07-03 13:33:19 +0000

    add: about-us

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -1,20 +1,44 @@
 @extends('layouts.app', ['title' => config('custom.title')])
 
 @section('main-content')
-    <h1 class="container mx-auto text-3xl font-bold underline">
-        About us
-    </h1>
-    <p class="container mx-auto">
-        Pellentesque habitant morbi tristique senectus et netus et malesuada fames
-        ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget,
-        tempor sit amet, ante. Donec eu libero sit amet quam egestas semper.
-        Aenean ultricies mi vitae est. Mauris placerat eleifend leo.
-        Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi,
-        condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum,
-        elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus
-        lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis.
-        Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue,
-        eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi,
-        tincidunt quis, accumsan porttitor, facilisis luctus, metus
-    </p>
+    <div class="container mx-auto p-4">
+        <div class="grid gap-4">
+            <div class="bg-white shadow-md rounded-lg overflow-hidden p-4">
+                <h2 class="text-xl font-bold mb-2">
+                    Nuestra Historia
+                </h2>
+
+                <p class="text-gray-700">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel urna quis urna fermentum bibendum.
+                </p>
+            </div>
+
+            <div class="bg-white shadow-md rounded-lg overflow-hidden p-4">
+                <h2 class="text-xl font-bold mb-2">
+                    Nuestro Equipo
+                </h2>
+                <p class="text-gray-700">
+                    Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+            </div>
+
+
+            <div class="container mx-auto p-4 bg-white rounded-lg shadow-lg">
+                <h2 class="text-xl font-bold mb-2">
+                    About Us
+                </h2>
+
+                <p class="text-gray-700 mb-4">
+                    We are a company dedicated to providing the best products and services to our
+                    customers...
+                </p>
+
+                <div class="flex space-x-4">
+                    <img src="path/to/image1.jpg" alt="Team member 1" class="w-1/4 rounded-lg">
+                    <img src="path/to/image2.jpg" alt="Team member 2" class="w-1/4 rounded-lg">
+                    <img src="path/to/image3.jpg" alt="Team member 3" class="w-1/4 rounded-lg">
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
