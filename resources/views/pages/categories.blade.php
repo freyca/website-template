277b5a7c96807b9076963e1b6e0650c878645f75
Fran Rey <fran.rc@raiolanetworks.es>
2024-07-08 18:32:13 +0000

    fix: margin

--- resources/views/pages/categories.blade.php
+++ resources/views/pages/categories.blade.php
@@ -3,7 +3,7 @@
     'metaDescription' => 'Metadescripcion de la pagina de categorias',
 ])
 @section('main-content')
-    <h1 class="mt-20 text-center text-3xl font-bold mb-4">
+    <h1 class="mt-5 text-center text-3xl font-bold mb-4">
         {{ __('Categories') }}
     </h1>
 
