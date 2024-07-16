fb537d8ead76870dbcf1f42ab2b8841b67bfb3a8
Fran Rey <fran.rc@raiolanetworks.es>
2024-07-16 13:32:04 +0000

    add: basic category card

--- resources/views/components/category/category-card.blade.php
+++ resources/views/components/category/category-card.blade.php
@@ -1,10 +1,11 @@
-<div class="bg-white mx-5 shadow-md rounded-lg p-6 hover:scale-105 transition-transform duration-300">
+<div class="bg-gray-200 mx-5 shadow-md rounded-lg p-6 hover:scale-105 transition-transform duration-300">
     <a href="{{ $category->slug }}">
-        <img class="w-full h-32 sm:h-48 object-cover" src="{{ @asset('/storage/' . $category->big_image) }}" />
+        <img class="h-32 sm:h-48 object-contain rounded-md mx-auto"
+            src="{{ @asset('/storage/' . $category->big_image) }}" />
     </a>
 
-    <div class="mt-4">
-        <h2 class="text-lg font-semibold">
+    <div class="mt-5">
+        <h2 class="text-lg font-semibold text-center">
             <a href="{{ $category->slug }}">
                 {{ $category->name }}
             </a>
