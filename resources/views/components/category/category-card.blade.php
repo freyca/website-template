32008502d69fd26c8d84a2284474bbeae7f7cce2
Fran Rey <fran.rc@raiolanetworks.es>
2024-07-22 14:43:48 +0000

    fix: category image size

--- resources/views/components/category/category-card.blade.php
+++ resources/views/components/category/category-card.blade.php
@@ -1,8 +1,7 @@
 <div
     class="bg-gray-600 mx-5 shadow-md rounded-lg p-6 hover:bg-gray-800 hover:scale-105 transition-transform duration-300">
     <a href="{{ $category->slug }}">
-        <img class="h-32 sm:h-48 object-contain rounded-t-md mx-auto"
-            src="{{ @asset('/storage/' . $category->big_image) }}" />
+        <img class="h-48 object-contain rounded-t-md mx-auto" src="{{ @asset('/storage/' . $category->big_image) }}" />
     </a>
 
     <div class="mt-5">
