3a8eb400c34e99b2447cad60e94ee256bb7580e2
Fran Rey <fran.rc@raiolanetworks.es>
2024-07-30 08:22:29 +0000

    add: better styling of sidebar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
 <body id="app-container" class="bg-gray-100 text-gray-900 min-h-screen m-0 flex flex-col">
     <x-navbar.navbar />
 
-    <div class="relative">
+    <div class="relative min-h-screen">
         <div class="container mx-auto mt-4 sm:p-4">
             {{ $slot }}
         </div>
