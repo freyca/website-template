ffb4e1736377ace0e3522d7e4d2ec301998cbe4c
Fran Rey <fran.rc@raiolanetworks.es>
2024-07-31 13:24:24 +0000

    fix: bug when showing features

--- resources/views/components/product/product-feature-container.blade.php
+++ resources/views/components/product/product-feature-container.blade.php
@@ -4,19 +4,27 @@
     </p>
 
     <div id="accordion-collapse" data-accordion="collapse" class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-1">
-        @foreach ($features->pluck('family', 'id')->unique() as $featureFamilyId => $featureFamily)
-            @foreach ($featureValues as $featureValue)
-                @if ($featureValue->product_feature_id === $featureFamilyId)
-                    <div class="">
-                        <h3 class="text-center p-5 font-md border bg-gray-200 border-gray-200 mt-6">
-                            {{ __($featureFamily->value) }}
-                        </h3>
+        {{-- Get associated families --}}
+        @foreach ($features->pluck('family')->unique() as $featureFamily)
+            <div class="">
+                {{-- Print name of family --}}
+                <h3 class="text-center p-5 font-md border bg-gray-200 border-gray-200 mt-6">
+                    {{ __($featureFamily->value) }}
+                </h3>
 
-                        <x-product.product-feature :feature="$features->where('id', $featureFamilyId)->first()" :featureValue="$featureValue" />
-                    </div>
-                @endif
-            @endforeach
+                @foreach ($features as $feature)
+                    {{-- Print the feature only if it belogns to family --}}
+                    @if ($feature->family === $featureFamily)
+                        @foreach ($featureValues as $featureValue)
+                            {{-- Print feature value only if belongs to feature --}}
+                            @if ($featureValue->product_feature_id === $feature->id)
+                                <x-product.product-feature :feature="$feature" :featureValue="$featureValue" />
+                            @endif
+                        @endforeach
+                    @endif
+                @endforeach
+
+            </div>
         @endforeach
     </div>
-
 </div>
