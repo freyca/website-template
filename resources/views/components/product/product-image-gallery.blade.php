63c1f8ecea54d2bb78ef34b25f38433ca13ddb27
TRSO <trsoluciones@outlook.es>
2024-08-05 01:57:36 +0200

    Corrección y mejora en los controles del slider de product-image-gallery.blade.php

--- resources/views/components/product/product-image-gallery.blade.php
+++ resources/views/components/product/product-image-gallery.blade.php
@@ -12,60 +12,90 @@
         }, @endforeach
     ],
     currentSlideIndex: 1,
+    mouseOverTimeout: null,
+    mouseLeaveTimeout: null,
     previous() {
-        if (this.currentSlideIndex > 1) {
-            this.currentSlideIndex = this.currentSlideIndex - 1
-        } else {
-            // If it's the first slide, go to the last slide
-            this.currentSlideIndex = this.slides.length
-        }
+        this.currentSlideIndex = (this.currentSlideIndex > 1) ? this.currentSlideIndex - 1 : this.slides.length;
     },
     next() {
-        if (this.currentSlideIndex < this.slides.length) {
-            this.currentSlideIndex = this.currentSlideIndex + 1
+        this.currentSlideIndex = (this.currentSlideIndex < this.slides.length) ? this.currentSlideIndex + 1 : 1;
+    },
+    handleMouseMove(event) {
+        const containerWidth = event.currentTarget.offsetWidth;
+        const mouseX = event.clientX;
+        const activationAreaWidth = 280;
+
+        clearTimeout(this.mouseLeaveTimeout);
+
+        if (mouseX < activationAreaWidth) {
+            document.getElementById('prevButton').style.width = '120px';
+        } else if (mouseX > containerWidth - activationAreaWidth) {
+            document.getElementById('nextButton').style.width = '120px';
         } else {
-            // If it's the last slide, go to the first slide
-            this.currentSlideIndex = 1
+            document.getElementById('prevButton').style.width = '30px';
+            document.getElementById('nextButton').style.width = '30px';
         }
     },
-}" class="relative w-full rounded-xl overflow-hidden">
+    handleMouseLeave() {
+        this.mouseLeaveTimeout = setTimeout(() => {
+            document.getElementById('prevButton').style.width = '30px';
+            document.getElementById('nextButton').style.width = '30px';
+        }, 300);
+    }
+}" class="relative w-full rounded-xl overflow-hidden shadow-xl bg-gray-100" @mousemove="handleMouseMove" @mouseleave="handleMouseLeave">
 
-    <!-- previous button -->
-    <button type="button"
-        class="absolute left-5 top-1/2 z-20 flex rounded-full -translate-y-1/2 items-center justify-center p-2 text-slate-700 transition focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-700 active:outline-offset-0"
-        aria-label="previous slide" x-on:click="previous()">
-        @svg('heroicon-s-arrow-left-circle', 'w-8 h-8')
+    <!-- Previous button for non-mobile view -->
+    <button id="prevButton" type="button" class="hidden md:flex absolute left-0 h-full top-0 z-20 items-center justify-center transition-all duration-300 focus:outline-none bg-opacity-80 bg-blue-800 hover:bg-black hover:bg-opacity-50" aria-label="previous slide" x-on:click="previous()" style="width: 30px;">
+        <svg class="w-8 h-8 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path>
+        </svg>
     </button>
 
-    <!-- next button -->
-    <button type="button"
-        class="absolute right-5 top-1/2 z-20 flex rounded-full -translate-y-1/2 items-center justify-center p-2 text-slate-700 transition focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-700 active:outline-offset-0"
-        aria-label="next slide" x-on:click="next()">
-        @svg('heroicon-s-arrow-right-circle', 'w-8 h-8')
+    <!-- Next button for non-mobile view -->
+    <button id="nextButton" type="button" class="hidden md:flex absolute right-0 h-full top-0 z-20 items-center justify-center transition-all duration-300 focus:outline-none bg-opacity-80 bg-blue-800 hover:bg-black hover:bg-opacity-40" aria-label="next slide" x-on:click="next()" style="width: 30px;">
+        <svg class="w-8 h-8 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
+        </svg>
     </button>
 
-    <!-- slides -->
-    <!-- Change min-h-[50svh] to your preferred height size -->
-    <div class="relative min-h-[50svh] w-full">
-        <template x-for="(slide, index) in slides">
-            <div x-show="currentSlideIndex == index + 1" class="absolute inset-0" x-transition.opacity.duration.1000ms>
-                <img class="absolute w-full h-full inset-0 object-contain text-slate-700" x-bind:src="slide.imgSrc"
+    <!-- Previous button for mobile view -->
+    <button type="button" class="md:hidden absolute left-4 top-1/2 transform -translate-y-1/2 z-20 flex items-center justify-center transition-all duration-300 focus:outline-none bg-opacity-80 bg-blue-800 hover:bg-opacity-50 hover:bg-black hover:shadow-lg rounded-full backdrop-blur-md" aria-label="previous slide" x-on:click="previous()" style="width: 60px; height: 60px; transform: scale(0.9);" @mouseover="event.currentTarget.style.transform='scale(1.1) rotate(-10deg)';" @mouseout="event.currentTarget.style.transform='scale(0.9)';">
+        <svg class="w-8 h-8 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path>
+        </svg>
+    </button>
+
+    <!-- Next button for mobile view -->
+    <button type="button" class="md:hidden absolute right-4 top-1/2 transform -translate-y-1/2 z-20 flex items-center justify-center transition-all duration-300 focus:outline-none bg-opacity-80 bg-blue-800 hover:bg-opacity-50 hover:bg-black hover:shadow-lg rounded-full backdrop-blur-md" aria-label="next slide" x-on:click="next()" style="width: 60px; height: 60px; transform: scale(0.9);" @mouseover="event.currentTarget.style.transform='scale(1.1) rotate(10deg)';" @mouseout="event.currentTarget.style.transform='scale(0.9)';">
+        <svg class="w-8 h-8 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
+        </svg>
+    </button>
+
+
+
+    <!-- Slides -->
+    <div class="relative min-h-[60vh] w-full">
+        <template x-for="(slide, index) in slides" :key="index">
+            <div x-show="currentSlideIndex == index + 1" class="absolute inset-0 transition-opacity duration-700"
+                x-transition.opacity>
+                <img class="absolute w-full h-full inset-0 object-cover" x-bind:src="slide.imgSrc"
                     x-bind:alt="slide.imgAlt" />
             </div>
         </template>
     </div>
 
-    <!-- indicators -->
-    <div class="absolute rounded-xl bottom-3 md:bottom-5 left-1/2 z-20 flex -translate-x-1/2 gap-4 md:gap-3 bg-white/75 px-1.5 py-1 md:px-2"
+    <!-- Indicators -->
+    <div
+        class="absolute rounded-xl bottom-4 left-1/2 z-20 flex -translate-x-1/2 gap-2 bg-opacity-80 bg-blue-800 px-2 py-1"
         role="group" aria-label="slides">
         <template x-for="(slide, index) in slides">
-            <button class="size-2 cursor-pointer rounded-full transition bg-slate-700"
+            <button class="w-3 h-3 cursor-pointer rounded-full transition "
                 x-on:click="currentSlideIndex = index + 1"
-                x-bind:class="[currentSlideIndex === index + 1 ? 'bg-slate-700' :
-                    'bg-slate-700/50'
-                ]"
+                x-bind:class="[currentSlideIndex === index + 1 ? 'bg-slate-700' : 'bg-white']"
                 x-bind:aria-label="'slide ' + (index + 1)">
             </button>
         </template>
     </div>
+
 </div>
