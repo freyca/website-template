2d4dacb567b54bbecc76abdf7862cb40a8c30964
Fran Rey <franreycastedo@gmail.com>
2025-01-16 16:16:15 +0100

    add: two columns only if more than one item in cart

--- resources/views/livewire/cart-items.blade.php
+++ resources/views/livewire/cart-items.blade.php
@@ -4,13 +4,20 @@
     </p>
     @inject('cart', 'App\Services\Cart')
 
-    @foreach ($cart->getCart() as $cartItem)
-        @livewire(
-            'cart.product-card',
-            [
-                'product' => data_get($cartItem, 'product'),
-            ],
-            key('product-' . data_get($cartItem, 'product.ean13'))
-        )
-    @endforeach
+    {{-- Do not show two columns with only one product --}}
+    @if($cart->getTotalQuantity() > 1)
+    <div class="xl:grid xl:grid-cols-2 xl:gap-2">
+    @else
+    <div>
+    @endif
+        @foreach ($cart->getCart() as $cartItem)
+            @livewire(
+                'cart.product-card',
+                [
+                    'product' => data_get($cartItem, 'product'),
+                ],
+                key('product-' . data_get($cartItem, 'product.ean13'))
+            )
+        @endforeach
+    </div>
 </div>
