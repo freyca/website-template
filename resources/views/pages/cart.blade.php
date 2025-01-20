91fbfc03763c16cc0254b878e09498e3fa0ba94c
Fran Rey <franreycastedo@gmail.com>
2025-01-20 19:45:50 +0100

    fix: cart padding

--- resources/views/pages/cart.blade.php
+++ resources/views/pages/cart.blade.php
@@ -1,7 +1,7 @@
 <x-layouts.app title="{{ config('custom.title') }}" metaDescription="Metadescripcion de la pagina de carrito">
     @inject('cart', 'App\Services\Cart')
 
-    <div class="mx-auto px-4 xl:px-0">
+    <div class="mx-auto px-0 xl:px-4">
         @livewire('cart.cart-container')
     </div>
 </x-layouts.app>
