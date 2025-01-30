a39c2c22775003c3bf697e40eca8a8d232f439c3
Fran Rey <franreycastedo@gmail.com>
2025-01-30 13:15:58 +0100

    add: shadow on floating buttons

--- resources/views/components/buttons/whats-app-button.blade.php
+++ resources/views/components/buttons/whats-app-button.blade.php
@@ -1,6 +1,6 @@
-<div class="duration-500 ease-in-out sticky p-1 h-auto z-[90] right-4 bottom-4 xl:right-40 xl:bottom-10 float-right">
+<div class="duration-500 ease-in-out sticky p-1 h-auto z-[90] right-4 bottom-4 xl:right-40 xl:bottom-6 float-right">
     <a href="https://api.whatsapp.com/send/?phone=34695489142" target="_blank">
-        <button id="whatsapp-contact" class="bg-green-500 text-white p-3 rounded-full" aria-label="Contactar por whatsapp">
+        <button id="whatsapp-contact" class="bg-green-500 text-white p-3 rounded-full shadow-lg shadow-green-300" aria-label="Contactar por whatsapp">
             @svg('ri-whatsapp-line', 'w-6 h-6')
         </button>
     </a>
