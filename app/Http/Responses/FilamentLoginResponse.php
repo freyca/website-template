70544dbacf214f9ea3f98ce8eb336d16adce293b
Fran Rey <franreycastedo@gmail.com>
2025-01-31 09:10:11 +0100

    add: when login, redirect to cart only customers, not admin users

--- app/Http/Responses/FilamentLoginResponse.php
+++ app/Http/Responses/FilamentLoginResponse.php
@@ -4,15 +4,22 @@ declare(strict_types=1);
 
 namespace App\Http\Responses;
 
+use App\Enums\Role;
 use App\Services\Cart;
 use Filament\Http\Responses\Auth\Contracts\LoginResponse;
 use Illuminate\Http\RedirectResponse;
 use Livewire\Features\SupportRedirects\Redirector;
+use Filament\Facades\Filament;
+use Illuminate\Support\Facades\Auth;
 
 class FilamentLoginResponse implements LoginResponse
 {
     public function toResponse($request): Redirector|RedirectResponse
     {
+        if (Auth::user()->role === Role::Admin) {
+            return redirect()->intended(Filament::getUrl());
+        }
+
         /** @var Cart */
         $cart = app(Cart::class);
 
