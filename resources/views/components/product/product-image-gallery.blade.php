4ef1edea53940b934011df55725ae2c9bf4c5a7e
Fran Rey <franreycastedo@gmail.com>
2025-02-20 15:02:36 +0100

    add: product gallery response to touch events

--- resources/views/components/product/product-image-gallery.blade.php
+++ resources/views/components/product/product-image-gallery.blade.php
@@ -1,4 +1,5 @@
 <div x-data="{
+    autoplayIntervalTime: 4000,
     slides: [
         {
             imgSrc: '{{ @asset('/storage/' . $product->main_image) }}',
@@ -19,67 +20,77 @@
         @endforeach
     ],
     currentSlideIndex: 1,
-    mouseOverTimeout: null,
-    mouseLeaveTimeout: null,
+    isPaused: false,
+    autoplayInterval: null,
+    touchStartX: null,
+    touchEndX: null,
+    swipeThreshold: 50,
     previous() {
-        this.currentSlideIndex = (this.currentSlideIndex > 1) ? this.currentSlideIndex - 1 : this.slides.length;
+        if (this.currentSlideIndex > 1) {
+            this.currentSlideIndex = this.currentSlideIndex - 1
+        } else {
+            // If it's the first slide, go to the last slide
+            this.currentSlideIndex = this.slides.length
+        }
     },
     next() {
-        this.currentSlideIndex = (this.currentSlideIndex < this.slides.length) ? this.currentSlideIndex + 1 : 1;
-    }
-}" class="relative w-full rounded-xl overflow-hidden shadow-xl bg-primary-100">
+        if (this.currentSlideIndex < this.slides.length) {
+            this.currentSlideIndex = this.currentSlideIndex + 1
+        } else {
+            // If it's the last slide, go to the first slide
+            this.currentSlideIndex = 1
+        }
+    },
+    handleTouchStart(event) {
+        this.touchStartX = event.touches[0].clientX
+    },
+    handleTouchMove(event) {
+        this.touchEndX = event.touches[0].clientX
+    },
+    handleTouchEnd() {
+        if(this.touchEndX){
+            if (this.touchStartX - this.touchEndX > this.swipeThreshold) {
+                this.next()
+            }
+            if (this.touchStartX - this.touchEndX < -this.swipeThreshold) {
+                this.previous()
+            }
+            this.touchStartX = null
+            this.touchEndX = null
+        }
+    },
+    autoplay() {
+        this.autoplayInterval = setInterval(() => {
+            if (! this.isPaused) {
+                this.next()
+            }
+        }, this.autoplayIntervalTime)
+    },
+    // Updates interval time
+    setAutoplayInterval(newIntervalTime) {
+        clearInterval(this.autoplayInterval)
+        this.autoplayIntervalTime = newIntervalTime
+        this.autoplay()
+    },
+}" x-init="autoplay" class="relative w-full overflow-hidden">
 
-    <!-- Previous button for non-mobile view -->
-    <button id="prevButton" type="button" class="hidden md:flex absolute left-0 h-full top-0 z-20 items-center justify-center transition-all duration-300 focus:outline-none bg-opacity-50 bg-slate-800" aria-label="previous slide" x-on:click="previous()" style="width: 50px;">
-        <svg class="w-8 h-8 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path>
-        </svg>
+    <!-- previous button -->
+    <button type="button" class="absolute left-5 top-1/2 z-20 flex -translate-y-1/2 items-center justify-center bg-surface/40 p-2 text-on-surface transition" aria-label="previous slide" x-on:click="previous()">
+        @svg('heroicon-c-chevron-left',  'bg-primary-300 text-primary-700 swiper-button-prev rounded-full')
     </button>
 
-    <!-- Next button for non-mobile view -->
-    <button id="nextButton" type="button" class="hidden md:flex absolute right-0 h-full top-0 z-20 items-center justify-center transition-all duration-300 focus:outline-none bg-opacity-50 bg-slate-800" aria-label="next slide" x-on:click="next()" style="width: 50px;">
-        <svg class="w-8 h-8 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
-        </svg>
+    <!-- next button -->
+    <button type="button" class="absolute right-5 top-1/2 z-20 flex rounded-full -translate-y-1/2 items-center justify-center bg-surface/40 p-2 text-on-surface transition" aria-label="next slide" x-on:click="next()">
+        @svg('heroicon-c-chevron-right', 'bg-primary-300 text-primary-700 swiper-button-next rounded-full')
     </button>
 
-    <!-- Previous button for mobile view -->
-    <button type="button" class="md:hidden absolute left-4 top-1/2 transform -translate-y-1/2 z-20 flex items-center justify-center transition-all duration-300 focus:outline-none bg-opacity-50 bg-slate-800 rounded-full backdrop-blur-md" aria-label="previous slide" x-on:click="previous()" style="width: 60px; height: 60px; transform: scale(0.9);" @mouseover="event.currentTarget.style.transform='scale(1.1) rotate(-10deg)';" @mouseout="event.currentTarget.style.transform='scale(0.9)';">
-        <svg class="w-8 h-8 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path>
-        </svg>
-    </button>
-
-    <!-- Next button for mobile view -->
-    <button type="button" class="md:hidden absolute right-4 top-1/2 transform -translate-y-1/2 z-20 flex items-center justify-center transition-all duration-300 focus:outline-none bg-opacity-50 bg-slate-800 rounded-full backdrop-blur-md" aria-label="next slide" x-on:click="next()" style="width: 60px; height: 60px; transform: scale(0.9);" @mouseover="event.currentTarget.style.transform='scale(1.1) rotate(10deg)';" @mouseout="event.currentTarget.style.transform='scale(0.9)';">
-        <svg class="w-8 h-8 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
-        </svg>
-    </button>
-
-
-
-    <!-- Slides -->
-    <div class="min-h-[40vh] sm:min-h-[50vh] md:min-h-[60vh] md:w-full">
-        <template x-for="(slide, index) in slides" :key="index">
-            <div x-show="currentSlideIndex == index + 1" class="absolute inset-0 transition-opacity duration-700"
-                x-transition.opacity>
-                <img class="absolute w-full h-full inset-0 object-cover" x-bind:src="slide.imgSrc"
-                    x-bind:alt="slide.imgAlt" />
-            </div>
-        </template>
-    </div>
-
-    <!-- Indicators -->
-    <div
-        class="absolute rounded-xl bottom-4 left-1/2 z-20 flex -translate-x-1/2 gap-2 bg-opacity-80 bg-slate-800 px-2 py-1"
-        role="group" aria-label="slides">
+    <!-- slides -->
+    <!-- Change min-h-[50svh] to your preferred height size -->
+    <div class="relative min-h-[40svh] w-full" x-on:touchstart="handleTouchStart($event)" x-on:touchmove="handleTouchMove($event)" x-on:touchend="handleTouchEnd()">
         <template x-for="(slide, index) in slides">
-            <button class="w-3 h-3 cursor-pointer rounded-full transition "
-                x-on:click="currentSlideIndex = index + 1"
-                x-bind:class="[currentSlideIndex === index + 1 ? 'bg-white' : 'bg-white/50']"
-                x-bind:aria-label="'slide ' + (index + 1)">
-            </button>
+            <div x-show="currentSlideIndex == index + 1" class="absolute inset-0" x-transition.opacity.duration.700ms>
+                <img class="rounded-md absolute w-full h-full inset-0 object-cover text-on-surface dark:text-on-surface-dark" x-bind:src="slide.imgSrc" x-bind:alt="slide.imgAlt" />
+            </div>
         </template>
     </div>
 
