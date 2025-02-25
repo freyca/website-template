252edea0b7358487b2d3712acdb9944ae2bb6c3c
Fran Rey <franreycastedo@gmail.com>
2025-02-25 12:46:43 +0100

    add: email notification with laravel default styles

--- app/Listeners/SendContactFormEmail.php
+++ app/Listeners/SendContactFormEmail.php
@@ -4,17 +4,21 @@ namespace App\Listeners;
 
 use App\Events\ContactFormSubmitted;
 use App\Mail\ContactForm;
+use App\Models\User;
+use App\Notifications\ContactFormNotification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Notification;
 
 class SendContactFormEmail implements ShouldQueue
 {
     public function handle(ContactFormSubmitted $event): void
     {
-        Mail::to(
-            config('custom.admin_email')
-        )->send(
-            new ContactForm($event->form)
+        Notification::send(
+            User::where('role', 'admin')->first(),     // TODO: define who will receive this notifications
+            new ContactFormNotification($event->form)
         );
+
+        return;
     }
 }
