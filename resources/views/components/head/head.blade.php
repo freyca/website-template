591058ba03121dfbe22377b56b1693f275f669eb
Fran Rey <franreycastedo@gmail.com>
2025-02-25 14:16:34 +0100

    add: fonts in css

--- resources/views/components/head/head.blade.php
+++ resources/views/components/head/head.blade.php
@@ -15,7 +15,6 @@
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Atkinson+Hyperlegible+Next:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
 
     @vite('resources/css/app.css')
     @filamentStyles
