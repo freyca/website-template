b8895dc23bcf0f81c84278b45c2ee62588108fec
Fran Rey <franreycastedo@gmail.com>
2025-03-03 19:43:54 +0100

    fix: centered text

--- resources/views/components/footer/footer.blade.php
+++ resources/views/components/footer/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="bg-primary-800 mt-auto rounded-t-lg">
     <div class="w-full max-w-screen-xl mx-auto py-2 sm:py-4">
         <div class="mx-4 sm:mx-10 flex items-center justify-between">
-            <a href="/" class="text-sm font-medium text-primary-300 mb-2 sm:mb-0 sm:flex items-center">
+            <a href="/" class="text-sm font-medium text-primary-300 mb-2 sm:mb-0 sm:flex items-center text-center">
                 <img src="{{ @asset('/images/logo.png') }}" class="h-8 mx-2" alt="Roteco" />
                 <span class="self-center text-xl sm:text-2xl font-semibold text-primary-50 whitespace-nowrap">
                     Roteco
