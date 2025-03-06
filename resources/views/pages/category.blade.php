4b63ec794cb3bec6d29c2831c57b31072e215078
Fran Rey <franreycastedo@gmail.com>
2025-03-06 22:17:37 +0100

    fix: styles

--- resources/views/pages/category.blade.php
+++ resources/views/pages/category.blade.php
@@ -4,7 +4,7 @@
         <x-bread-crumbs :breadcrumbs="$breadcrumbs" />
 
         <div class="grid grid-cols-1 mt-4 lg:gap-4 lg:grid-cols-3 mb-4">
-            <div class="grid place-content-center flex bg-primary-800 rounded mx-4">
+            <div class="flex bg-primary-800 rounded mx-4">
                 <figure class="p-6">
                     <picture>
                         <img class="h-56" src="{{ @asset('/storage/' . $category->big_image) }}">
