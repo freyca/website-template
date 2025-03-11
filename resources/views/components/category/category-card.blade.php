42fb245bce1dd254a4a227eff4da834b54fc8b1c
Fran Rey <franreycastedo@gmail.com>
2025-03-11 17:33:14 +0100

    fix: styles in category card

--- resources/views/components/category/category-card.blade.php
+++ resources/views/components/category/category-card.blade.php
@@ -1,13 +1,13 @@
 <div class="bg-primary-800 rounded-md relative flex m-2">
-    <a href="{{ $category->slug }}" class="h-full">
+    <a href="{{ $category->slug }}" class="h-full mx-auto">
         <figure class="p-6 pb-10">
             <picture>
                 <img class="h-20 md:h-40" src="{{ @asset('/storage/' . $category->big_image) }}" alt="{{ $category->name }}">
             </picture>
 
-            <figcaption class="absolute bottom-0 right-0">
-                <h3 class="text-lg font-semibold text-primary-800">
-                    <span class="bg-white py-2 px-4 rounded-tl-md">
+            <figcaption class="absolute bottom-0 right-0 max-w-64 text-right">
+                <h3 class="text-lg font-semibold text-primary-800 rounded-tl-md truncate">
+                    <span class="bg-white py-2 px-2">
                         {{ $category->name }}
                     </span>
                 </h3>
