f13a3c55c646f1994890c30df070b9a6f67f9420
Fran Rey <franreycastedo@gmail.com>
2025-03-11 18:08:01 +0100

    fix: truncate breadcrumbs

--- resources/views/components/bread-crumbs.blade.php
+++ resources/views/components/bread-crumbs.blade.php
@@ -8,13 +8,13 @@
                     </a>
                 </li>
             @else
-                <li @if($loop->last) {{ 'aria-current=page' }} @endif >
-                    <div class="flex items-center">
+                <li @if($loop->last) {{ 'aria-current=page class=truncate' }} @endif>
+                    <div class="flex items-center truncate">
                         @svg('heroicon-c-chevron-right', 'h-4 w-4 text-primary-800 text-semibold')
                         @if(! $loop->last)
                             <a href="{{$url}}" class="ms-1 text-sm font-medium text-primary-700 hover:text-primary-600 md:ms-2">{{ $breadcrumb }}</a>
                         @else
-                            <span class="ms-1 text-sm font-medium text-primary-500 md:ms-2">{{ $breadcrumb }}</span>
+                            <span class="ms-1 text-sm font-medium text-primary-500 md:ms-2 truncate">{{ $breadcrumb }}</span>
                         @endif
                     </div>
                 </li>
