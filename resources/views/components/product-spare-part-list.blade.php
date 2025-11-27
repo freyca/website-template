d3ee197cb7d844631fbfa4066553dab3b6f50eb6
Fran Rey <franreycastedo@gmail.com>
2025-11-27 21:52:47 +0100

    some styling

--- resources/views/components/product-spare-part-list.blade.php
+++ resources/views/components/product-spare-part-list.blade.php
@@ -1,7 +1,7 @@
 <div class="ml-3">
     <ul>
     @foreach ($relatedSpareparts as $spare_part)
-        <li class="p-1 grid grid-cols-2">
+        <li class="p-1 grid grid-cols-[1fr_auto] gap-2 items-center">
             <div class="flex items-center text-left">⚙️ - {{ $spare_part->name }}</div>
             @livewire('buttons.product-cart-buttons', ['product' => $spare_part, collect() ])
         </li>
