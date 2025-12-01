bf9aac738d09a8592eb4ef7c5cb8b6f4497d5a24
Fran Rey <franreycastedo@gmail.com>
2025-12-01 16:58:02 +0100

    add: testing product urls

--- tests/Feature/ProductUrlsTest.php
+++ tests/Feature/ProductUrlsTest.php
@@ -1,128 +1,56 @@
 <?php
 
 use App\Models\Product;
-use App\Models\ProductComplement;
 use App\Models\ProductSparePart;
 use App\Models\User;
-use Database\Seeders\ProductComplementSeeder;
 use Database\Seeders\ProductSeeder;
 use Database\Seeders\ProductSparePartSeeder;
 use Database\Seeders\UserAdminSeeder;
-use Database\Seeders\UserSeeder;
 
 use function Pest\Laravel\actingAs;
 use function Pest\Laravel\get;
 
 test('product urls returns 200 if published and 403 if not', function () {
     $this->seed(ProductSeeder::class);
+    $user = User::factory()->create();
 
     $publishedProducts = Product::where('published', true)->get();
 
     foreach ($publishedProducts as $product) {
-        $response = get('/producto/'.$product->slug);
+        $response = actingAs($user)->get('/producto/' . $product->slug);
         $response->assertStatus(200);
     }
 
     $notPublishedProducts = Product::where('published', false)->get();
 
     foreach ($notPublishedProducts as $product) {
-        $response = get('/producto/'.$product->slug);
-        $response->assertStatus(403);
-    }
-})->group('product-urls');
-
-test('product complement urls returns 200 if published and 403 if not', function () {
-    $this->seed(ProductComplementSeeder::class);
-    $this->seed(ProductSeeder::class);
-
-    $publishedComplements = ProductComplement::where('published', true)->get();
-
-    foreach ($publishedComplements as $product) {
-        $response = get('/complemento/'.$product->slug);
-        $response->assertStatus(200);
-    }
-
-    $notPublishedComplements = ProductComplement::where('published', false)->get();
-
-    foreach ($notPublishedComplements as $product) {
-        $response = get('/complemento/'.$product->slug);
-        $response->assertStatus(403);
-    }
-})->group('product-urls');
-
-test('product spare part urls returns 200 if published and 403 if not', function () {
-    $this->seed(ProductSparePartSeeder::class);
-    $this->seed(ProductSeeder::class);
-
-    $publishedSpareParts = ProductSparePart::where('published', true)->get();
-
-    foreach ($publishedSpareParts as $product) {
-        $response = get('/pieza-de-repuesto/'.$product->slug);
-        $response->assertStatus(200);
-    }
-
-    $notPublishedSpareParts = ProductSparePart::where('published', false)->get();
-
-    foreach ($notPublishedSpareParts as $product) {
-        $response = get('/pieza-de-repuesto/'.$product->slug);
+        $response = get('/producto/' . $product->slug);
         $response->assertStatus(403);
     }
 })->group('product-urls');
 
 test('admin can access published and not published products', function () {
     $this->seed(ProductSparePartSeeder::class);
-    $this->seed(ProductComplementSeeder::class);
     $this->seed(ProductSeeder::class);
-    $this->seed(UserAdminSeeder::class);
+
+    $user = User::factory()->admin()->create();
 
     $products = Product::all();
-    $productComplements = ProductComplement::all();
-    $productSpareParts = ProductSparePart::all();
-
-    /** @var User */
-    $user = User::first();
 
     foreach ($products as $product) {
-        $response = actingAs($user)->get('/producto/'.$product->slug);
-        $response->assertStatus(200);
-    }
-
-    foreach ($productComplements as $product) {
-        $response = actingAs($user)->get('/complemento/'.$product->slug);
-        $response->assertStatus(200);
-    }
-
-    foreach ($productSpareParts as $product) {
-        $response = actingAs($user)->get('/pieza-de-repuesto/'.$product->slug);
+        $response = actingAs($user)->get('/producto/' . $product->slug);
         $response->assertStatus(200);
     }
 })->group('product-urls');
 
-test('logged user cannnot access not published products', function () {
+test('spare parts urls cannot be accesed directly', function () {
     $this->seed(ProductSparePartSeeder::class);
-    $this->seed(ProductComplementSeeder::class);
-    $this->seed(ProductSeeder::class);
-    $this->seed(UserSeeder::class);
+    $user = User::factory()->create();
 
-    $products = Product::where('published', false)->get();
-    $productComplements = ProductComplement::where('published', false)->get();
-    $productSpareParts = ProductSparePart::where('published', false)->get();
-
-    /** @var User */
-    $user = User::first();
+    $products = ProductSparePart::all();
 
     foreach ($products as $product) {
-        $response = actingAs($user)->get('/producto/'.$product->slug);
-        $response->assertStatus(403);
-    }
-
-    foreach ($productComplements as $product) {
-        $response = actingAs($user)->get('/complemento/'.$product->slug);
-        $response->assertStatus(403);
-    }
-
-    foreach ($productSpareParts as $product) {
-        $response = actingAs($user)->get('/pieza-de-repuesto/'.$product->slug);
-        $response->assertStatus(403);
+        $response = actingAs($user)->get('/pieza-de-repuesto/' . $product->slug);
+        $response->assertStatus(404);
     }
 })->group('product-urls');
