25e006a3d5f52e2219fd7229c9a9695e79d60fc9
Fran Rey <franreycastedo@gmail.com>
2025-12-03 16:36:30 +0100

    add: spare-parts shows prices

--- resources/views/components/product-spare-part-list.blade.php
+++ resources/views/components/product-spare-part-list.blade.php
@@ -1,8 +1,16 @@
 <div class="ml-3">
     <ul>
     @foreach ($relatedSpareparts as $spare_part)
-        <li class="p-1 grid grid-cols-[1fr_auto] gap-2 items-center">
-            <div class="flex items-center text-left">⚙️ - {{ $spare_part->name }}</div>
+        <li class="p-1 flex gap-2 items-center">
+            <div class="flex-1 flex items-center text-left">⚙️ - {{ $spare_part->name }}</div>
+                @if($spare_part->price_with_discount)
+                    <div class="flex items-center text-left font-bold text-green-600">
+                    {{ $spare_part->getFormattedPriceWithDiscount() }}
+                @else
+                    <div class="flex items-center text-left font-bold">
+                    {{ $spare_part->getFormattedPrice() }}
+                @endif
+            </div>
             @livewire('buttons.product-cart-buttons', ['product' => $spare_part, collect() ])
         </li>
     @endforeach
